e868bbdcb21f37e1ec1214b3f7648467ab0150cd
IfeCoker <93936451+IfeCoker@users.noreply.github.com>
2022-06-07 13:36:18 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 // complete the function
 function prime (num) {
   // code goes here
+  for (var x = 2; x < num; x++) {
+    if (num % x === 0) {
+      return false;
+    }
+  }
+  return true;
 }
 
 function solution (arg) {
